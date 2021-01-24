a59dd07641b15642c4d1d78cdad62a4dd4e38dd0
Mohammad <pr.mlz2020@gmail.com>
2021-01-24 20:20:49 +0200

    setting up things

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useRef } from 'react';
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer, CircleGeometry, MeshBasicMaterial, Mesh } from 'three';
 
 const setup = (scene: Scene, camera: PerspectiveCamera) => {
   console.log("setup");
-  const geometry = new BoxGeometry();
+  const geometry = new CircleGeometry(1,32);
   const material = new MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new Mesh(geometry, material);
-  scene.add(cube);
+  const ball = new Mesh(geometry, material);
+  scene.add(ball);
 
-  camera.position.z = 5;
+  camera.position.z = 20;
 
-  return { cube };
+  return { ball };
 }
 
-const animationLoop = (cube: Mesh) => {
-  cube.rotation.x += 0.05;
-  cube.rotation.y += 0.03;
+const animationLoop = (ball: Mesh) => {
+  ball.position.x += 0.01;
 }
 
 const App: React.FC = () => {
@@ -26,14 +25,14 @@ const App: React.FC = () => {
     const renderer = new WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    const { cube } = setup(scene, camera);
+    const { ball } = setup(scene, camera);
     const element = document.getElementById('__container')!;
     const canvas = element.children[0];
     element.replaceChild(renderer.domElement, canvas);
     
     let animate = () => {
       requestAnimationFrame(animate);
-      animationLoop(cube);
+      animationLoop(ball);
       renderer.render(scene, camera);
     }
     animate();
