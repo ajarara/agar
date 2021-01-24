a55de3ba27e1488d5b72f0c161c473be4ea81cd8
Ahmad Jarara <ajarara94@gmail.com>
2021-01-24 11:31:01 -0500

    Recreate the canvas on source change
    Previously, we would replace the div we had on the screen with a
    canvas on the first run, and then the div would be gone, so we would
    never replace it, even when the effect re-ran.
    
    Now, recreate the whole thing, every time.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,50 @@
 import React, { useEffect, useRef } from 'react';
-import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
 
-const animationLoop = () => {
-  
+const setup = (scene: Scene, camera: PerspectiveCamera) => {
+  console.log("setup");
+  const geometry = new BoxGeometry();
+  const material = new MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  return { cube };
+}
+
+const animationLoop = (cube: Mesh) => {
+  cube.rotation.x += 0.05;
+  cube.rotation.y += 0.03;
 }
 
 const App: React.FC = () => {
-  const scene = useRef(new Scene());
-  const camera = useRef(new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000));
-  const renderer = useRef(new WebGLRenderer());
-  renderer.current!.setSize( window.innerWidth, window.innerHeight);
 
   useEffect(() => {
-    const element = document.getElementById('__canvas');
-    element?.parentNode?.replaceChild(renderer.current!.domElement, element!);
-    const animate = () => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const { cube } = setup(scene, camera);
+    const element = document.getElementById('__container')!;
+    const canvas = element.children[0];
+    element.replaceChild(renderer.domElement, canvas);
+    
+    let animate = () => {
       requestAnimationFrame(animate);
-      animationLoop();
-      renderer.current!.render(scene.current!, camera.current!);
+      animationLoop(cube);
+      renderer.render(scene, camera);
     }
     animate();
   }, []);
   
   return (
-    <div id={'__canvas'}/>
+    <>
+      <div id={'__container'}>
+        <div />
+      </div>
+    </>
   );
 }
 
