362b586f017aeb7161ed1fe6d01693b8cb2b8eeb
Ahmad Jarara <ajarara94@gmail.com>
2021-01-24 13:41:57 -0500

    Split initial canvas and animation in two

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Scene, PerspectiveCamera, WebGLRenderer, CircleGeometry, MeshBasicMaterial, Mesh } from 'three';
 
 const setup = (scene: Scene, camera: PerspectiveCamera) => {
@@ -17,18 +17,37 @@ const animationLoop = (ball: Mesh) => {
   ball.position.x += 0.01;
 }
 
+type SceneState = {
+  scene: Scene,
+  camera: PerspectiveCamera,
+  renderer: WebGLRenderer
+}
+
 const App: React.FC = () => {
+  const [sceneState, setSceneState] = useState<SceneState | undefined>(undefined);
 
   useEffect(() => {
+    console.log("Scene state creation");
     const scene = new Scene();
     const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    const { ball } = setup(scene, camera);
+
     const element = document.getElementById('__container')!;
     const canvas = element.children[0];
     element.replaceChild(renderer.domElement, canvas);
+    setSceneState({ scene, camera, renderer });
+  }, []);
+
+  useEffect(() => {
+    console.log("Scene state listening!");
+    if (!sceneState) {
+      return;
+    }
+    console.log("Scene state set!");
+    const { scene, camera, renderer } = sceneState;
+    const { ball } = setup(scene, camera);
     
     let animate = () => {
       requestAnimationFrame(animate);
@@ -36,7 +55,7 @@ const App: React.FC = () => {
       renderer.render(scene, camera);
     }
     animate();
-  }, []);
+  }, [sceneState]);
   
   return (
     <>
